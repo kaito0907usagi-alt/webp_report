950bdcf88c2f9b9fc68db0f4e16f81da0768f27b
大菅海兎 <kaito0907usagi@gmail.com>
2025-12-28 23:04:14 +0900

    提出用完成

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
+app.use("/style", express.static(__dirname + "/style"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
